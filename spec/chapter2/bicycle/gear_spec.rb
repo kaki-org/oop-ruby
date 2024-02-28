24ded1280dafb219561e66754397f3438ec35604
mpg-teruo-kakikubo <t.kakikubo@fitsplus.co.jp>
2024-02-29 07:19:38 +0900

    初期設定を済ませてみる

---
+++ spec/chapter2/bicycle/gear_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+# require './gear'
+
+describe Gear do
+  let(:gear) { Gear.new(52, 11) }
+
+  context 'ギアが52,コグが11の場合' do
+    it 'ギア比が4.7272727272727275であること' do
+      expect(gear.ratio).to eq 4.7272727272727275
+    end
+  end
+end
