96596c903817ad3e9ab3a21e1770b2d918135d93
kakikubo@gmail.com <kakikubo@gmail.com>
2024-02-29 07:39:48 +0900

    rubocop -a

--- spec/chapter2/bicycle/gear_spec.rb
+++ spec/chapter2/bicycle/gear_spec.rb
@@ -6,7 +6,6 @@ describe Gear do
   context 'ギアが52,コグが11の場合' do
     let(:gear) { Gear.new(52, 11) }
 
-
     it 'ギア比が4.7272727272727275であること' do
       expect(gear.ratio).to eq 4.7272727272727275
     end
@@ -14,10 +13,8 @@ describe Gear do
   context 'ギアが30,コグが27の場合' do
     let(:gear) { Gear.new(30, 27) }
 
-
     it 'ギア比が1.1111111111111112であること' do
       expect(gear.ratio).to eq 1.1111111111111112
     end
   end
-
 end
