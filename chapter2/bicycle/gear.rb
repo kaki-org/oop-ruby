73fca07445bd5d468d7ca584fe4ba26fd52e35b1
kakikubo@gmail.com <kakikubo@gmail.com>
2024-03-01 05:27:11 +0900

    ギアインチの計算を実装する(但し、テストは失敗する)

--- chapter2/bicycle/gear.rb
+++ chapter2/bicycle/gear.rb
@@ -2,14 +2,24 @@
 
 # Gearクラスを定義
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def gear_inches
+    # ギアインチ = 車輪の直径 ✕ ギア比
+    # 車輪の直径 = リムの直径 ＋ タイヤの厚みの2倍
+    #
+    # タイヤはリムの周りを囲むので、直径を計算するためには2倍する
+    (rim + (tire * 2)) * ratio
   end
 end
