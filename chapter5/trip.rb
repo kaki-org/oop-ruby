fc602d0d6e8785da861ceb797b4c0e72f1cbe982
kakikubo@gmail.com <kakikubo@gmail.com>
2024-03-06 06:48:32 +0900

    整備しに加え、旅行の準備にコーディネーターと運転手も含まれるようになった

--- chapter5/trip.rb
+++ chapter5/trip.rb
@@ -1,13 +1,41 @@
+# 旅行の準備が複雑になった
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
   # この 'mechanic' 引数はどんなクラスのものでも良い
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(preparers)
+    preparers.each do |preparer|
+      case preparer
+      when Mechanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vehicle)
+      end
+    end
   end
 end
 
-# *この*クラスのインスタンスを渡す事になったとしても動作する
+# TripCoordinatorとDriverクラスも追加された
+class TripCoordinator
+  def buy_food(customers)
+    # ...
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    # ...
+  end
+
+  def fill_water_tank(vehicle)
+    # ...
+  end
+end
+
+# Mechanicクラスも追加された
 class Mechanic
   def prepare_bicycles(bicycles)
     bicycles.each { |bicycle| prepare_bicycle(bicycle) }
