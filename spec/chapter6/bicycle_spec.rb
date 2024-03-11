91f1ca8721576cbe8ef718479396db9724e009e5
kakikubo@gmail.com <kakikubo@gmail.com>
2024-03-12 07:16:07 +0900

    Revert "マウンテンバイクの詳細指定バージョンのテストも記載"
    This reverts commit 49a54557b09f4aaa911f412f29790416b6dfff26.

--- spec/chapter6/bicycle_spec.rb
+++ spec/chapter6/bicycle_spec.rb
@@ -35,7 +35,7 @@ describe Bicycle do
     end
   end
 
-  context 'マウンテンバイクで何も指定しない場合' do
+  context 'マウンテンバイクの場合' do
     let(:bicycle) { described_class.new(style: :mountain) }
 
     it 'タイヤサイズが2.1であること' do
@@ -48,26 +48,6 @@ describe Bicycle do
 
     it 'リアショックがnilであること' do
       expect(bicycle.rear_shock).to be_nil
-    end
-  end
-
-  context 'マウンテンバイクの詳細を指定した場合' do
-    let(:bicycle) { described_class.new(style: :mountain, size: 'S', front_shock: 'Manitou', rear_shock: 'Fox') }
-
-    it 'タイヤサイズが2.1であること' do
-      expect(bicycle.spares[:tire_size]).to eq '2.1'
-    end
-
-    it 'チェーンが10-speedであること' do
-      expect(bicycle.spares[:chain]).to eq '10-speed'
-    end
-
-    it 'フロントショックがManitouであること' do
-      expect(bicycle.front_shock).to eq 'Manitou'
-    end
-
-    it 'リアショックがFoxであること' do
-      expect(bicycle.rear_shock).to eq 'Fox'
     end
   end
 end
