4f6eb6535a9a2b0c41df749b2d69e11dd59895b2
kakikubo@gmail.com <kakikubo@gmail.com>
2024-03-14 05:49:16 +0900

    rubocopの警告を削除

--- spec/chapter6/mountain_bike_spec.rb
+++ spec/chapter6/mountain_bike_spec.rb
@@ -3,7 +3,7 @@
 require 'rspec'
 
 describe MountainBike do
-  context 'Sサイズのマウンテンバイク' do
+  context 'Sサイズのマウンテンバイクの場合' do
     let(:mountain_bike) { described_class.new(size: 'S', front_shock: 'Manitou', rear_shock: 'Fox') }
 
     it 'サイズがSであること' do
@@ -14,7 +14,6 @@ describe MountainBike do
       expect(mountain_bike.spares).to eq(
         chain: '10-speed',
         tire_size: '23', # 間違い
-        # tape_color: nil, # 不適切
         rear_shock: 'Fox',
         front_shock: 'Manitou'
       )
