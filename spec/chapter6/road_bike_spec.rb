7120fd506927fe499896ce0af580966392b7af3b
kakikubo@gmail.com <kakikubo@gmail.com>
2024-03-14 06:06:34 +0900

    適切な変数名に変更

--- spec/chapter6/road_bike_spec.rb
+++ spec/chapter6/road_bike_spec.rb
@@ -4,34 +4,34 @@ require 'rspec'
 
 describe RoadBike do
   context '何も指定しない場合' do
-    let(:bicycle) { described_class.new }
+    let(:road_bike) { described_class.new }
 
     it 'タイヤサイズが23であること' do
-      expect(bicycle.spares[:tire_size]).to eq '23'
+      expect(road_bike.spares[:tire_size]).to eq '23'
     end
 
     it 'チェーンが10-speedであること' do
-      expect(bicycle.spares[:chain]).to eq '10-speed'
+      expect(road_bike.spares[:chain]).to eq '10-speed'
     end
 
     it 'テープカラーがnilであること' do
-      expect(bicycle.tape_color).to be_nil
+      expect(road_bike.tape_color).to be_nil
     end
   end
 
   context 'Mサイズでテープカラーが赤の場合' do
-    let(:bicycle) { described_class.new(size: 'M', tape_color: 'red') }
+    let(:road_bike) { described_class.new(size: 'M', tape_color: 'red') }
 
     it 'タイヤサイズが23であること' do
-      expect(bicycle.spares[:tire_size]).to eq '23'
+      expect(road_bike.spares[:tire_size]).to eq '23'
     end
 
     it 'チェーンが10-speedであること' do
-      expect(bicycle.spares[:chain]).to eq '10-speed'
+      expect(road_bike.spares[:chain]).to eq '10-speed'
     end
 
     it 'テープカラーが赤であること' do
-      expect(bicycle.tape_color).to eq 'red'
+      expect(road_bike.tape_color).to eq 'red'
     end
   end
 end
