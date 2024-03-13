f91d7a2ff603e2901c4f6b7e1fd857117c47ebec
kakikubo@gmail.com <kakikubo@gmail.com>
2024-03-14 06:17:19 +0900

    recumbentでのsuperを忘れずに実装した場合

--- chapter6/recumbent_bike.rb
+++ chapter6/recumbent_bike.rb
@@ -4,9 +4,9 @@
 class RecumbentBike < Bicycle
   attr_reader :flag
 
-  def initialize(size: nil, chain: '9-speed', flag: nil)
-    @flag = flag # superを忘れる
-    # super(size:, chain:)
+  def initialize(size: nil, chain: '9-speed', tire_size: nil, flag: nil)
+    @flag = flag # superを忘れると途端にsizeやchainがnilになるので注意
+    super(size:, chain:, tire_size:)
   end
 
   def spares
