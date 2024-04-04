f49d8b1408b434ca08ed9fe580f0fca00216cbe0
kakikubo@gmail.com <kakikubo@gmail.com>
2024-04-05 07:47:36 +0900

    Chapter2のクラスと競合しないように修正

---
+++ chapter2/gear.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Chapter2
+  # Gearクラスを定義
+  class Gear
+    attr_reader :chainring, :cog, :wheel, :opt_bool
+
+    def initialize(chainring: 40, cog: 18, wheel: nil, opt_bool: true)
+      @chainring = chainring
+      @cog = cog
+      @wheel = wheel
+      @opt_bool = opt_bool
+    end
+
+    def ratio
+      chainring / cog.to_f
+    end
+
+    def gear_inches
+      # ギアインチ = 車輪の直径 ✕ ギア比
+      diameter * ratio
+    end
+
+    private
+
+    def diameter
+      wheel.diameter
+    end
+  end
+end
