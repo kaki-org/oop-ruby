f45ea3817a9d91832f3df4da3f875302bea1d7ac
kakikubo@gmail.com <kakikubo@gmail.com>
2024-04-05 07:34:46 +0900

    第2章(3章?)ででてきたコードを再掲
    それぞれ、受信メッセージと送信メッセージは以下の通りにまとめることができる
    
    オブジェクト | 受信メッセージ | 送信メッセージ | 依存されているか？
    -- | -- | -- | --
    Wheel | diameter | | Yes
    Gear | | diameter | No
    Gear | gear_inches | | Yes
    Gear | ratio | | Yes

---
+++ chapter9/gear.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Gearクラスを定義
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring: nil, cog: nil, rim: nil, tire: nil)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def gear_inches
+    # ギアインチ = ギア比 ✕ 車輪の直径
+    ratio * Wheel.new(rim, tire).diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
