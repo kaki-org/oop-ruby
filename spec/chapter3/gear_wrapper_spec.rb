cead902f0226becbc8b33b93f747a440500f4d3b
kakikubo@gmail.com <kakikubo@gmail.com>
2024-04-06 07:30:11 +0900

    テストダブルを第3章のテストコードに実装する

--- spec/chapter3/gear_wrapper_spec.rb
+++ spec/chapter3/gear_wrapper_spec.rb
@@ -28,4 +28,17 @@ describe GearWrapper do
       expect(gear.gear_inches).to eq 137.0909090909091
     end
   end
+
+  context 'テストダブルを用いる場合' do
+    let(:wheel) { instance_double('Wheel', diameter: 10) }
+    let(:gear) { described_class.gear(chainring: 52, cog: 11, wheel:) }
+
+    it 'ギア比が4.7272727272727275であること' do
+      expect(gear.ratio).to eq 4.7272727272727275
+    end
+
+    it 'ギアインチが47.27272727272727であるここと' do
+      expect(gear.gear_inches).to eq 47.27272727272727
+    end
+  end
 end
