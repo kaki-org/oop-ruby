026ac2a5b348ce4bfb4d9b1236af9c0af0cd6c28
kakikubo@gmail.com <kakikubo@gmail.com>
2024-04-06 09:18:16 +0900

    wheelはdiameterを呼び出せることを検証する

--- spec/chapter9/wheel_spec.rb
+++ spec/chapter9/wheel_spec.rb
@@ -6,6 +6,10 @@ describe ::Wheel do
   let(:wheel) { described_class.new(rim: 26, tire: 1.5) }
 
   context 'diameterがよばれたとき' do
+    it 'そもそもdiameterがあること' do
+      expect(wheel).to respond_to(:diameter)
+    end
+
     it '直径が正しいこと' do
       expect(wheel.diameter).to eq 29.0
     end
