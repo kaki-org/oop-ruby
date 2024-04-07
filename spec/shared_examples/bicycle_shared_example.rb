90de88454a3979bcaed3db03f4c323d84449d747
kakikubo@gmail.com <kakikubo@gmail.com>
2024-04-08 06:18:16 +0900

    moduleをChapter6として定義した。
    スタブを用意し、仮想的なサブクラスを用いてsparesのテストをする
    サブクラス側でsparesのテストまで実装してしまうのは冗長だといいたいみたい

--- spec/shared_examples/bicycle_shared_example.rb
+++ spec/shared_examples/bicycle_shared_example.rb
@@ -1,12 +1,10 @@
+# frozen_string_literal: true
+
 # 共通のテストを定義
 # 書籍ではBicycleInterfaceTestとしていたもの
 shared_examples 'a bicycle' do
   it 'responds to default_chain' do
     expect(subject).to respond_to(:default_chain)
-  end
-
-  it 'responds to default_tire_size' do
-    expect(subject).to respond_to(:default_tire_size)
   end
 
   it 'responds to chain' do
--- spec/shared_examples/bicycle_subclass_example.rb
+++ spec/shared_examples/bicycle_subclass_example.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 # サブクラスのテストを共通化するための共有テストを定義する
 # BicycleSubclassTestとして書籍で扱われていたもの
 shared_examples 'a bicycle subclass' do
+  it 'responds to default_tire_size' do
+    expect(subject).to respond_to(:default_tire_size)
+  end
+
   it 'responds to post_initialize' do
     expect(subject).to respond_to(:post_initialize)
   end
